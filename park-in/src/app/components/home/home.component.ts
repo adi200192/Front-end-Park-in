198b250fa827a326ec90ae7d774a4a8d0942d736
hamza <hamza.cherkaoui1@etu.univ-grenoble-alpes.fr>
2025-01-20 15:45:18 +0100

    problem with dependecies

--- park-in/src/app/components/home/home.component.ts
+++ park-in/src/app/components/home/home.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,          
+    MatDatepickerModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    MatNativeDateModule,
+    FormsModule,
+    MatTimepickerModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent { 
+  location: string = '';
+  dateDebut: Date | null = null;
+  dateFin: Date | null = null;
 
+  onSearch() {
+    console.log('Lieu de stationnement:', this.location);
+    console.log('Date de début:', this.dateDebut);
+    console.log('Date de fin:', this.dateFin);
 }
+}
+
