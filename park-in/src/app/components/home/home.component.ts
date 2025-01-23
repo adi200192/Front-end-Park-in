fd302ca1959371ff979ddab7cae26b17f90a1d6a
hamza <hamza.cherkaoui1@etu.univ-grenoble-alpes.fr>
2025-01-23 18:02:16 +0100

    adding list parking

--- park-in/src/app/components/home/home.component.ts
+++ park-in/src/app/components/home/home.component.ts
@@ -1,36 +1,22 @@
 import {AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { InputTextModule } from 'primeng/inputtext';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { CheckboxModule } from 'primeng/checkbox';
-import {MessageModule} from 'primeng/message';
-import {DropdownModule} from 'primeng/dropdown';
 import {MatFormField, MatLabel, MatSelect} from '@angular/material/select';
 import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
-import {PrimeNGConfig} from 'primeng/api';
 import {ApiService} from '../../services/api.service';
 import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatInputModule} from '@angular/material/input';
 import {MatTimepickerModule} from '@angular/material/timepicker';
 import {MatButtonModule} from '@angular/material/button';
+import {Router} from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   imports: [
     CommonModule,
-    ButtonModule,
-    FloatLabelModule,
-    InputTextModule,
     ReactiveFormsModule,  // Use only ReactiveFormsModule
-    CalendarModule,
-    CheckboxModule,
-    MessageModule,
-    DropdownModule,
     MatDatepickerModule,
     MatTimepickerModule,
     MatButtonModule,
@@ -47,7 +33,6 @@ import {MatButtonModule} from '@angular/material/button';
   standalone: true,
   providers: []
 })
-
 export class HomeComponent implements AfterViewInit{
   searchForm: FormGroup;
   formSubmitted: boolean = false;
@@ -55,48 +40,34 @@ export class HomeComponent implements AfterViewInit{
   @ViewChild('autoCompleteInput') autoCompleteInput!:ElementRef
 
   typePlaces = [
-    {label : 'Deux roues', value: 'deux_roues'},
-    {label : 'Deux roues électriques', value : 'deux_roues_electrique' },
-    {label : 'Electrique', value: 'electrique'},
-    {label : 'Standard', value : 'standard'}
+    {label : 'Deux roues', value: 'DEUX_ROUES'},
+    {label : 'Deux roues électriques', value : 'DEUX_ROUES_ELECTRIQUE' },
+    {label : 'Electrique', value: 'ELECTRIQUE'},
+    {label : 'Standard', value : 'STANDARD'},
+    {label : 'Vélo', value : 'VELO'}
   ]
 
   typeParkings = [
-    {label : 'Enclos en surface', value : 'enclos_en_surface'},
-    {label : 'Ouvrage', value : 'ouvrage'}
+    {label : 'Enclos en surface', value : 'ENCLOS_EN_SURFACE'},
+    {label : 'Ouvrage', value : 'TYPE_OUVRAGE'}
   ]
-  constructor(private primengConfig : PrimeNGConfig, private apiService : ApiService) {
+  constructor(private apiService : ApiService, private router : Router) {
     this.searchForm = new FormGroup({
       location: new FormControl('', Validators.required),
       dateDebut: new FormControl(null, Validators.required),
       latitude: new FormControl(null),
       longitude: new FormControl(null),
       dateFin: new FormControl(null, Validators.required),
-      isPmr: new FormControl(false),
-      typePlace : new FormControl('standard'),
-      typeParking : new FormControl('enclos_en_surface'),
-      hauteur : new FormControl()
+      pmr: new FormControl(false),
+      type : new FormControl('standard'),
+      typeOuvrage : new FormControl('enclos_en_surface'),
+      hauteur : new FormControl(1.90)
     });
 
-    this.primengConfig.setTranslation({
-      firstDayOfWeek: 1,
-      dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
-      dayNamesShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
-      dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
-      monthNames: [
-        "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
-        "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
-      ],
-      monthNamesShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Juin", "Juil", "Août", "Sep", "Oct", "Nov", "Déc"],
-      today: "Aujourd'hui",
-      clear: "Effacer",
-      dateFormat: 'dd/mm/yy',
-      weekHeader: 'Sem'
-    });
   }
  autocomplete : google.maps.places.Autocomplete | undefined
   ngAfterViewInit() {
-    this.autocomplete = new google.maps.places.Autocomplete(this.autoCompleteInput.nativeElement);
+    this.autocomplete = new google.maps.places.Autocomplete(this.autoCompleteInput.nativeElement, { componentRestrictions: { country: 'fr' }});
 
     this.autocomplete.addListener('place_changed', ()=> {
       const place = this.autocomplete?.getPlace();
@@ -125,6 +96,7 @@ export class HomeComponent implements AfterViewInit{
     if (this.searchForm.valid) {
       this.formSubmitted = true
       this.testGeocoding(this.searchForm.get('location')?.value)
+      this.router.navigate(['/search'])
     } else {
       console.log('Form is invalid!');
     }
