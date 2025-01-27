917786c1a5d92e2fabbeb565131cb6ac403e970f
hamza <hamza.cherkaoui1@etu.univ-grenoble-alpes.fr>
2025-01-27 11:28:58 +0100

    removing duplicata

--- park-in/src/app/app.routes.ts
+++ park-in/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CancelComponent } from './components/cancel/cancel.component';
-import {SearchComponent} from './components/search/search.component';
 import {BookingComponent} from './components/booking/booking.component';
 import { SearchComponent } from './components/search/search.component';
 
